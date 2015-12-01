40fa96015bbcf35bd513dd5437cbcd69ab64dcf4
r159 <rah>
2015-12-02 00:16:08 +0530

    Converting Video(Mp4) to Audio files in C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace V2A
+{
+    partial class Video2Audio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Video2Audio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source Files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination Folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 89);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 211);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(311, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(225, 211);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(157, 212);
+            this.listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(439, 211);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(153, 212);
+            this.listBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "No of Files:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Processing";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(482, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Processed";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::V2A.Properties.Resources.arrownew;
+            this.pictureBox1.Location = new System.Drawing.Point(176, 283);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 34);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::V2A.Properties.Resources.arrownew;
+            this.pictureBox2.Location = new System.Drawing.Point(387, 283);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 34);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(262, 140);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(150, 490);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(363, 95);
+            this.listBox4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 467);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(363, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Files Not Processed Because of Size Limit(size should be less than 120 MB)";
+            // 
+            // Video2Audio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 597);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Video2Audio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Video2Audio";
+            this.Load += new System.EventHandler(this.Video2Audio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace V2A
+{
+    public partial class Video2Audio : Form
+    {
+        List<string> FilePaths = new List<string>();
+        public string folderPath = @"C:\Ffmeg\newoutput\";
+        OrderedDictionary  FileNamePath = new OrderedDictionary();
+        List<string> avoidedFiles = new List<string>();
+        public Video2Audio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           BackgroundWorker bg2 = new BackgroundWorker();
+           bg2.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+           bg2.RunWorkerAsync();
+            
+            if((textBox2.Text != "") || (textBox2.Text != null) && FileNamePath.Count > 1)
+            {
+                     ProcessV2a();
+                     
+            }
+            else
+            {
+                MessageBox.Show("Please select Destination foler and atleast one file to convert");
+
+            }
+         
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label6.Visible = false;
+            listBox4.Visible = false;
+            this.listBox1.Items.Clear();
+             OpenFileDialog fileDialog = new OpenFileDialog();
+            
+            fileDialog.InitialDirectory = @"C:\";
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "Upload Files";
+           
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                int count = fileDialog.FileNames.Count();
+                label3.Text = label3.Text + count.ToString();
+                foreach (string item in fileDialog.FileNames)
+                {
+                    FileInfo f = new FileInfo(item);
+                    long length =  f.Length;
+                    long lengthinMB = (length / (1024 * 1024));
+                    if (lengthinMB > 120)
+                    {
+                       // avoidedFiles.Add(Path.GetFileName(item));
+                        label6.Visible = true;
+                        listBox4.Visible = true;
+                        listBox4.Items.Add(Path.GetFileName(item));
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(Path.GetFileName(item));
+                        FilePaths.Add(item);
+                        FileNamePath.Add(Path.GetFileName(item), item);
+                    }
+
+                }
+       
+
+            }
+            
+
+        }
+
+        private async void ProcessV2a()
+        {
+           
+            Task[] tasks = new Task[10];
+            int filescount = FileNamePath.Count;
+            object[] keys = new object[FileNamePath.Keys.Count];
+            FileNamePath.Keys.CopyTo(keys, 0);
+            if (filescount <= 10) {
+                for (int taskNumber =1; taskNumber <= filescount;)
+                {
+                  string filename = keys[taskNumber-1].ToString();
+                  string filepath = FileNamePath[taskNumber-1].ToString();
+                    tasks[taskNumber-1] = Task.Factory.StartNew(() => SendFilesToffmpeg(filename,filepath));
+                    taskNumber = taskNumber + 1;
+                     
+                }
+                textBox2.Text = "";
+                FileNamePath.Clear();
+               
+            }
+            else
+            {
+                int taskNumber =0;
+                int tasklimit = 10;
+                int startTask = 1;
+                int curretiteration =0;
+                int addnumber = 0;
+                int adnumber1 = 1;
+                int filestemp = 0;
+                float noofiteration = (filescount / 10F);
+                double noofiterations = Math.Ceiling(noofiteration);
+                while (curretiteration < noofiterations)
+                {
+
+                    if (noofiterations > 1 && curretiteration != 0)
+                    {
+                        int tempcurretiteration = curretiteration + 1;
+                        //int tempcurr2 = curretiteration + 2;
+                        tasklimit = int.Parse(tempcurretiteration.ToString() + addnumber.ToString());  //curretiteration + Int16.Parse("0");
+                        tasklimit = tasklimit > filescount ? filescount : tasklimit;
+                        tasklimit = tasklimit - 10;
+                        tasks = new Task[tasklimit];
+                        startTask = int.Parse(curretiteration.ToString() + adnumber1.ToString());
+                        startTask = startTask > tasklimit ? (startTask - 10) : startTask;
+
+                    }
+
+                    for (taskNumber = startTask; taskNumber <= tasklimit;)
+                    {
+                        string filename = keys[filestemp].ToString();
+                        string filepath = FileNamePath[filestemp].ToString();
+                        tasks[taskNumber - 1] = Task.Factory.StartNew(() => SendFilesToffmpeg(filename, filepath));
+                        taskNumber = taskNumber + 1;
+                        filestemp = filestemp + 1;
+                    }
+                  //  Task.WaitAll();
+                    curretiteration = curretiteration + 1;
+                   // Task.WaitAll();
+                    await Task.WhenAll(tasks);
+                    //for (int i = 1; i < tasks.Count(); i++)
+                    //{
+                    //    if (tasks[i] != null)
+                    //        tasks[i].Wait();
+                    //}
+                }
+                textBox2.Text = "";
+                FileNamePath.Clear();
+               
+            }
+           
+        }
+
+        private void SendFilesToffmpeg(string filename,string filepath)
+        {
+           
+            try {
+                //MessageBox.Show("From SendFiles" + FileNamePath.Count.ToString());
+               
+           
+        
+            //this.listBox2.Items.Add(filename);
+            this.Invoke((Action)(() => listBox2.Items.Add(filename)));
+            string filenamewithoutext = filename.Remove(filename.IndexOf("."));
+            string convertedfilename = filenamewithoutext + ".mp3";
+            string outpupath = folderPath + "\\" +convertedfilename;
+            string cmmd = " -i \"" + filepath + "\" \"" + outpupath + "\"";
+            //-i input -vn -f mp3 -ab 192k output.mp3
+
+            //string filepathexe = @"C:\Ffmeg\ffmpeg.exe";
+            string app = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string[] getapp = app.Split(new string[] { "bin" }, StringSplitOptions.None);
+            string ffmpegpath = getapp[0].ToString() + "ffmpeg" + "\\" + "ffmpeg.exe";
+            string filepathexe = ffmpegpath;
+            //InsertLogstotable(filepathexe);
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+
+            proc.StartInfo.FileName = filepathexe;
+
+            //Path of exe that will be executed, only for "filebuffer" it will be "flvtool2.exe"
+
+            proc.StartInfo.Arguments = cmmd;
+
+            //The command which will be executed
+
+            proc.StartInfo.UseShellExecute = false;
+
+            proc.StartInfo.CreateNoWindow = true;
+
+            proc.StartInfo.RedirectStandardOutput = false;
+
+            proc.Start();
+
+            while (proc.HasExited == false)
+            {
+
+            }
+           }
+            
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+              //  this.listBox3.Items.Add(filename);
+                this.Invoke((Action)(() => listBox3.Items.Add(filename)));
+                //myForm.Invoke((Action)(() => listBox3.Items.Clear()));
+                //this.Invoke((Action)(() => listBox3.Items.Clear()));
+                this.Invoke((Action)(() => listBox2.Items.Remove(filename)));
+                this.Invoke((Action)(() => listBox1.Items.Remove(filename)));
+
+                
+
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+           
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = folderPath;
+            }
+        }
+
+        private void Video2Audio_Load(object sender, EventArgs e)
+        {
+           // string applicationDirectory = Application.ExecutablePath;
+            label6.Visible = false;
+            listBox4.Visible = false;
+           
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bool islistboxOneContains = true;
+            while (islistboxOneContains)
+            {
+                if(listBox1.Items.Count >= 1)
+                {
+
+                      this.Invoke((Action)(() => button2.Enabled = false));
+                    this.Invoke((Action)(() =>button3.Enabled = false));
+                    this.Invoke((Action)(() =>button1.Enabled = false));
+                }
+                else
+                {
+                    this.Invoke((Action)(() =>button2.Enabled = true));
+                    this.Invoke((Action)(() =>button3.Enabled = true));
+                    this.Invoke((Action)(() =>button1.Enabled = true));
+                    islistboxOneContains = false;
+                }
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileNamePath.Clear();
+            textBox2.Text = "";
+            listBox1.Items.Clear();
+            label6.Visible = false;
+            listBox4.Visible = false;
+        }
+    }
+}
